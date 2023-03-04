384f96718083cb6aa0b8e6863323898a5014a8ab
aawubadughaa <101138338+aawubadughaa@users.noreply.github.com>
2023-03-03 19:06:01 -0500

    Update drowsiness.py

--- drowsiness.py
+++ drowsiness.py
@@ -1,79 +1,91 @@
-import cv2
-import dlib
 import numpy as np
+import cv2
+import os
+import dlib
+from scipy.spatial import distance as dist
+import tensorflow as tf
 
-# Initialize dlib's face detector and landmark predictor
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
-# Define the eye aspect ratio (EAR) threshold to detect drowsiness
-EAR_THRESHOLD = 0.2
-
-# Define a function to calculate the EAR of each eye
+# define the eye aspect ratio function
 def eye_aspect_ratio(eye):
-    A = np.linalg.norm(eye[1]-eye[5])
-    B = np.linalg.norm(eye[2]-eye[4])
-    C = np.linalg.norm(eye[0]-eye[3])
-    return (A+B)/(2.0*C)
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
 
-# Initialize the video stream and grab the first frame
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
-
-# Define the initial values for the EAR and consecutive frames with closed eyes
-EAR = 0
-COUNTER = 0
-
-# Start the main loop
-while True:
-    # Read a new frame from the video stream
-    ret, frame = cap.read()
-
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces in the grayscale frame
+# define the function to extract eye features from a photo
+def extract_eye_features(photo_file):
+    img = cv2.imread(photo_file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
     faces = detector(gray, 0)
+    eye_features = []
 
-    # Loop over the faces
     for face in faces:
-        # Detect the landmarks of the face
-        shape = predictor(gray, face)
-        shape = np.array([[p.x, p.y] for p in shape.parts()])
+        landmarks = predictor(gray, face)
+        left_eye = []
+        right_eye = []
 
-        # Extract the left and right eye landmarks and calculate the EAR for each eye
-        left_eye = shape[36:42]
-        right_eye = shape[42:48]
-        left_EAR = eye_aspect_ratio(left_eye)
-        right_EAR = eye_aspect_ratio(right_eye)
+        for n in range(36, 42):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            left_eye.append((x, y))
 
-        # Calculate the average EAR for both eyes
-        EAR = (left_EAR + right_EAR) / 2.0
+        for n in range(42, 48):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            right_eye.append((x, y))
 
-        # Check if the EAR is below the threshold to detect drowsiness
-        if EAR < EAR_THRESHOLD:
-            # Increment the counter for consecutive frames with closed eyes
-            COUNTER += 1
+        left_ear = eye_aspect_ratio(left_eye)
+        right_ear = eye_aspect_ratio(right_eye)
+        ear = (left_ear + right_ear) / 2.0
+        eye_features.append(ear)
 
-            # If the counter exceeds a certain threshold, alert the user that they are drowsy
-            if COUNTER >= 25:
-                cv2.putText(frame, "DROWSY", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        else:
-            # Reset the counter if the eyes are open
-            COUNTER = 0
+    return eye_features
 
-        # Draw the landmarks and the EAR for both eyes on the frame
-        cv2.drawContours(frame, [left_eye], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [right_eye], -1, (0, 255, 0), 1)
-        cv2.putText(frame, "EAR: {:.2f}".format(EAR), (450, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+# define the function to load the data
+def load_data(data_dir):
+    eye_features = []
+    labels = []
 
-    # Show the frame
-    cv2.imshow("Frame", frame)
+    for filename in os.listdir(data_dir):
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            photo_file = os.path.join(data_dir, filename)
+            features = extract_eye_features(photo_file)
+            eye_features.append(features)
 
-    # Check for the "q" key to exit the program
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            if 'awake' in filename.lower() or 'open' in filename.lower():
+                label = 0
+            else:
+                label = 1
+            labels.append(label)
 
-# Clean up
-cap.release()
-cv2.destroyAllWindows()
+    eye_features = np.array(eye_features)
+    labels = np.array(labels)
+    return eye_features, labels
+
+# load the training and testing data
+train_data_dir = 'train_photos'
+test_data_dir = 'test_photos'
+train_eye_features, train_labels = load_data(train_data_dir)
+test_eye_features, test_labels = load_data(test_data_dir)
+
+# define the tensorflow model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+# compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# train the model
+model.fit(train_eye_features, train_labels, epochs=10, validation_data=(test_eye_features, test_labels))
+
+# evaluate the model
+test_loss, test_acc = model.evaluate(test_eye_features, test_labels)
+print('Test accuracy:', test_acc)
+
+#http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
